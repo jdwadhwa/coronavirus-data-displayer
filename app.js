29a71e9a1934b85441fa38328301ebe0a640278a
jdwadhwa <jatinwadhwa@gmail.com>
2020-08-27 17:39:45 +0530

    some new changes

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var bodyParser = require('body-parser')
 const app  = express();
 
 
-const port = process.env.Port||3000;
+const port = process.env.PORT||3000;
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
